c2c3b7c4d84df47e23f87b99a067103a27f870f3
Jim Katz <jimk@tbrc.org>
2018-12-20 12:43:42 -0500

    First template
    Signed-off-by: Jim Katz <jimk@tbrc.org>

--- audit/urls.py
+++ audit/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # ex: /audit/
     path('', views.index, name = "indexYY"),
     # /audit/3
-    path('<int:submittal_id>', views.submittal, name = "Submittal"),
+    path('submittal/<int:submittal_id>', views.submittal, name = "Submittal"),
     # ex: /audit/work/5
     path('work/<int:work_id>', views.work, name = "work"),
     # ex: '/audit/contrib/3
--- audit/views.py
+++ audit/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Submittal
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Howdy volks")
+    # Do it the long way first
+    template = loader.get_template('audit/submittals.html')
+    context = {'list_name': 'Submittals',
+                'submittals': Submittal.objects.all(), }
+    return HttpResponse(template.render(context, request))
 
 def work(request, work_id: int):
     return HttpResponse(f"looking at  work {work_id}")
